41a312752442d95031f4e6c2901ece2b0a0b9e66
mattrochford <38470982+mattrochford@users.noreply.github.com>
2022-11-08 13:04:44 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,8 @@ def main():
   streamlit.write('Thanks for adding ', add_my_fruit)
 
   # Add row to table
-  my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('from streamlit')")
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('from streamlit')")
 
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
