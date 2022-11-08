5c56030affd9ff9f8676a39a74b59a1e0c6ec721
mattrochford <38470982+mattrochford@users.noreply.github.com>
2022-11-08 09:06:54 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,11 @@ streamlit.dataframe(fruits_to_show)
 # New section for fruityvice API requests
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+# Prompt for user input
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 # Convert the response to a dataframe
